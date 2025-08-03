8c9dc4702f25aed36997ef6620a86f97d4f88158
Eren <seneren1905@gmail.com>
2025-08-03 15:10:14 +0300

    Delete securityFilterChain method

--- src/main/java/com/mayadem/battlearena/api/config/SecurityConfig.java
+++ src/main/java/com/mayadem/battlearena/api/config/SecurityConfig.java
@@ -2,13 +2,9 @@ package com.mayadem.battlearena.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -24,17 +20,5 @@ public class SecurityConfig {
 
     
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.POST, "/api/warriors/register", "/api/warriors/login").permitAll()
-                .anyRequest().authenticated()
-            );
-            
-
-        return http.build();
-    }
+   
 }
