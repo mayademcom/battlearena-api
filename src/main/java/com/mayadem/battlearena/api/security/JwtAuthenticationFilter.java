ac21cba103f5670ffc6e5e7a86ede82c81c59e6b
Eren <seneren1905@gmail.com>
2025-08-05 20:14:23 +0300

    add @component again for experiment

--- src/main/java/com/mayadem/battlearena/api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/mayadem/battlearena/api/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.mayadem.battlearena.api.entity.Warrior;
@@ -17,7 +18,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
