14b3a560b3c27ee275ec22291c76b401c67aee1b
Eren <seneren1905@gmail.com>
2025-08-06 11:01:10 +0300

    create initial GET /profile endpoint structure

--- src/main/java/com/mayadem/battlearena/api/controller/WarriorController.java
+++ src/main/java/com/mayadem/battlearena/api/controller/WarriorController.java
@@ -2,6 +2,7 @@ package com.mayadem.battlearena.api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mayadem.battlearena.api.dto.LoginRequest;
 import com.mayadem.battlearena.api.dto.LoginResponse;
+import com.mayadem.battlearena.api.dto.WarriorProfileDto;
 import com.mayadem.battlearena.api.dto.WarriorRegistrationRequest;
 import com.mayadem.battlearena.api.dto.WarriorRegistrationResponse;
 import com.mayadem.battlearena.api.service.WarriorService;
@@ -35,4 +37,9 @@ public class WarriorController {
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         return ResponseEntity.ok(warriorService.login(request));
     }
+    
+    @GetMapping("/profile")
+    public ResponseEntity<WarriorProfileDto> getAuthenticatedWarriorProfile() {
+            return new ResponseEntity<>(HttpStatus.OK);
+}
 }
