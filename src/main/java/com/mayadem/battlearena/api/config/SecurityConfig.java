f492d0d4252a0216408061208e8d824141beab70
Eren <seneren1905@gmail.com>
2025-08-06 11:51:57 +0300

    protect the GET /profile endpoint

--- src/main/java/com/mayadem/battlearena/api/config/SecurityConfig.java
+++ src/main/java/com/mayadem/battlearena/api/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
     @Bean
     public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilterRegistrationBean(JwtAuthenticationFilter filter) {
         FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>(filter);
+        registrationBean.addUrlPatterns("/api/warriors/profile");
         return registrationBean;
     }
 
