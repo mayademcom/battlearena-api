cacdf9ac8fc8931e37ece58b3b01be2a863c0640
Eren <seneren1905@gmail.com>
2025-08-07 18:25:46 +0300

    extract authenticated principal for profile update

--- src/main/java/com/mayadem/battlearena/api/controller/WarriorController.java
+++ src/main/java/com/mayadem/battlearena/api/controller/WarriorController.java
@@ -3,6 +3,7 @@ package com.mayadem.battlearena.api.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -53,7 +54,9 @@ public class WarriorController {
 
     @PutMapping("/profile")
     public ResponseEntity<WarriorProfileDto> updateAuthenticatedWarriorProfile(@Valid @RequestBody UpdateProfileRequestDto requestDto) {
-
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Warrior authenticatedWarrior = (Warrior) authentication.getPrincipal();
+        String username = authenticatedWarrior.getUsername();
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
