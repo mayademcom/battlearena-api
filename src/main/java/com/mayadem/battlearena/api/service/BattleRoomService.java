8fb75b2f459d355cd5ae1cc93113ef9613eb2fe1
Eren <seneren1905@gmail.com>
2025-08-15 14:46:17 +0300

    feat(service): implement unique room code generation

--- src/main/java/com/mayadem/battlearena/api/service/BattleRoomService.java
+++ src/main/java/com/mayadem/battlearena/api/service/BattleRoomService.java
@@ -1,5 +1,8 @@
 package com.mayadem.battlearena.api.service;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +22,33 @@ public class BattleRoomService {
 
     @Transactional
     public BattleRoomDto createBattleRoom(StartBattleRequestDto requestDto, Warrior creator) {
-
+        String roomCode = generateUniqueRoomCode();
         return null;
+    }
+
+    private String generateUniqueRoomCode() {
+
+        final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        final Random random = new SecureRandom();
+
+        final StringBuilder codeBuilder = new StringBuilder(6);
+
+        String roomCode;
+        do {
+
+            codeBuilder.setLength(0);
+
+            for (int i = 0; i < 6; i++) {
+
+                int randomIndex = random.nextInt(ALPHABET.length());
+
+                codeBuilder.append(ALPHABET.charAt(randomIndex));
+            }
+            roomCode = codeBuilder.toString();
+
+        } while (battleRoomRepository.findByRoomCode(roomCode).isPresent());
+
+        return roomCode;
     }
 }
