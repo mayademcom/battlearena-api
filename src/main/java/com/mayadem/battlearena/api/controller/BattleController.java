10032a635fce5735520775c60bf0672fee54331e
Eren <seneren1905@gmail.com>
2025-08-15 15:49:08 +0300

    implement endpoint for joining a battle

--- src/main/java/com/mayadem/battlearena/api/controller/BattleController.java
+++ src/main/java/com/mayadem/battlearena/api/controller/BattleController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/battles")
@@ -34,6 +35,16 @@ public class BattleController {
         BattleRoomDto createdBattleRoom = battleRoomService.createBattleRoom(requestDto, creator);
 
         return new ResponseEntity<>(createdBattleRoom, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{roomCode}/join")
+    public ResponseEntity<BattleRoomDto> joinBattle(@PathVariable String roomCode) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Warrior joiningWarrior = (Warrior) authentication.getPrincipal();
+
+        BattleRoomDto updatedRoom = battleRoomService.joinBattleRoom(roomCode, joiningWarrior);
+
+        return new ResponseEntity<>(updatedRoom, HttpStatus.OK);
     }
 
 }
