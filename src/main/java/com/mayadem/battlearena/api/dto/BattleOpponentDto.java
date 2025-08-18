c72c7e46d37a2a8633d1b7ecd54048d3e569fc7c
Dila Adagideli <dilaadagideli@gmail.com>
2025-08-18 23:38:20 +0300

    updated dtos

--- src/main/java/com/mayadem/battlearena/api/dto/BattleOpponentDto.java
+++ src/main/java/com/mayadem/battlearena/api/dto/BattleOpponentDto.java
@@ -1,45 +1,59 @@
 package com.mayadem.battlearena.api.dto;
 
+import com.mayadem.battlearena.api.entity.enums.BattleResult;
+
 public class BattleOpponentDto {
     private String username;
     private String displayName;
     private int score;
-    private String result; // e.g., "WIN" or "LOSS"
+    private BattleResult result; // e.g., "WIN" or "LOSS"
     private int rankPointsBefore;
     private int rankPointsAfter;
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getDisplayName() {
         return displayName;
     }
+
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
+
     public int getScore() {
         return score;
     }
+
     public void setScore(int score) {
         this.score = score;
     }
-    public String getResult() {
+
+    public BattleResult getResult() {
         return result;
     }
-    public void setResult(String result) {
+
+    public void setResult(BattleResult result) {
         this.result = result;
     }
+
     public int getRankPointsBefore() {
         return rankPointsBefore;
     }
+
     public void setRankPointsBefore(int rankPointsBefore) {
         this.rankPointsBefore = rankPointsBefore;
     }
+
     public int getRankPointsAfter() {
         return rankPointsAfter;
     }
+
     public void setRankPointsAfter(int rankPointsAfter) {
         this.rankPointsAfter = rankPointsAfter;
     }
